189c83ad624bce512511512af0c7a8f0666d753c
Diorgenes Morais <diorgenes.m.silva@accenture.com>
2024-05-15 22:14:57 -0300

    refactor: change id type to int

--- src/main/kotlin/com/dms/messages/model/Message.kt
+++ src/main/kotlin/com/dms/messages/model/Message.kt
@@ -1,5 +1,6 @@
 package com.dms.messages.model
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
@@ -11,6 +12,7 @@ import jakarta.persistence.Table
 data class Message (
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long,
+    @JsonProperty(value = "id",  access = JsonProperty.Access.READ_ONLY)
+    val id: Int,
     val text: String
 )
--- src/main/kotlin/com/dms/messages/repository/MessagesRepository.kt
+++ src/main/kotlin/com/dms/messages/repository/MessagesRepository.kt
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface MessagesRepository : JpaRepository<Message, Long> {
+interface MessagesRepository : JpaRepository<Message, Int> {
 }
