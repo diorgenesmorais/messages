d88478bd373662d2ff34ffc80bdeb09c1a0d1dcf
Diorgenes Morais <diorgenes.m.silva@accenture.com>
2024-05-15 22:12:16 -0300

    feat: create message repository

---
+++ src/main/kotlin/com/dms/messages/repository/MessagesRepository.kt
@@ -0,0 +1,9 @@
+package com.dms.messages.repository
+
+import com.dms.messages.model.Message
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface MessagesRepository : JpaRepository<Message, Long> {
+}
